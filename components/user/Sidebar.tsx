ca8cf19b2ac0472e306e0a0196c685f462c9f419
talha kazmi <talhakazmi694@gmail.com>
2025-02-04 11:40:15 +0500

    fixed the logo color

--- components/user/Sidebar.tsx
+++ components/user/Sidebar.tsx
@@ -40,7 +40,7 @@ export default function Sidebar({
           <div className="relative w-36 h-8 lg:w-44 lg:h-10">
             <Link href="/admin">
               <Image
-                 src="/assests/EcomFillLogo.png"
+               src="/assests/logowhite.png"
                 alt="Admin Logo"
                 fill
                 className="object-contain"
